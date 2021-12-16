da68b6497a84bed27573cbe18e0d98a5c7b79f73
aadhaarbhatia.99@gmail.com <PremBhatia1@PREMs-MacBook-Air-3322.local>
2021-12-17 00:21:58 +0530

    E-commerce

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys')
+
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({ hi: "there @c19sn" });
-})
+    res.send({ Web: "feedbag-dev $devops" });
+});
+
+/*
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+}, (accessToken) => {
+    console.log(accessToken);
+}));
+
+app.get(
+    '/auth/google',
+    passport.authenticate('google', {
+        scope: ['profile', 'email']
+    }));
+
+app.get(
+    '/auth/google/callback',
+    (req, res) => {
+        res.send({ Devops: "Dev with ITops/Dataops" });
+    });
 
 const PORT = process.env.PORT || 5020
 app.listen(PORT, console.log("Listenting @5020.."));
+*/
