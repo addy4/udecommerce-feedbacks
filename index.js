9f170917facb321afa70e3976e87497199d8c88d
aadhaarbhatia.99@gmail.com <PremBhatia1@PREMs-MacBook-Air-3322.local>
2021-12-29 01:58:16 +0530

    E-commerce

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express');
-//const passport = require('passport');
-//const GoogleStrategy = require('passport-google-oauth20').Strategy;
-//const keys = require('./config/keys')
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys')
 
 const app = express();
-
 app.get('/', (req, res) => {
     res.send({ Web: "feedbag-dev as limitlessShore via herokuGit" });
 });
@@ -17,20 +16,18 @@ passport.use(new GoogleStrategy({
 }, (accessToken) => {
     console.log(accessToken);
 }));
-
 app.get(
     '/auth/google',
     passport.authenticate('google', {
         scope: ['profile', 'email']
     }));
-
 app.get(
     '/auth/google/callback',
     (req, res) => {
-        res.send({ Devops: "Dev with ITops/Dataops" });
+        res.send({ Devops: "Dev with Dataops" });
     });
-*/
 
+*/
 const PORT = process.env.PORT || 5020
 app.listen(PORT, console.log("Listenting @5020.."));
 
