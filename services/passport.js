7c24a19256d765510ee5eab13aa7ae96437bac96
aadhaarbhatia.99@gmail.com <PremBhatia1@PREMs-MacBook-Air-3322.local>
2022-03-19 15:39:21 +0530

    proxy set true for heroku deployment

--- services/passport.js
+++ services/passport.js
@@ -21,7 +21,8 @@ passport.deserializeUser((id, done) => {
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
+    callbackURL: '/auth/google/callback',
+    proxy: true
 }, (accessTk, refreshToken, profile, done) => {
     console.log('accessToken: ', accessTk);
     console.log('refreshToken: ', refreshToken);
