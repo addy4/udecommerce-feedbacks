9aba899d97b846c2d8756756af4488591e234349
Aadhaar Bhatia <aadhaar.bhatia@non.keysight.com>
2023-10-24 17:11:49 +0530

    added dev config vars

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+    googleClientID: '593796747245-hc5m996mqg1eg1k50itf35qvnafibn6p.apps.googleusercontent.com',
+    googleClientSecret: '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',
+    mongoURI: 'mongodb+srv://aadhaarb4:addymymongodei@udecommerce-db.jeayk.mongodb.net/udecommerce-db?retryWrites=true&w=majority',
+    cookieEncryptionKey: 'zxcsnwpofwevkwfpecnefcasnrqoqcpwklkwbxjc',
+    stripePublishableKey: 'pk_test_51NEG1xSBK5SfV7xSF1iy2NPCzJCTV5vNutV0bBM03im7VhrihYxZxdNKB65Yg2Uv7nQW9NkZlPhQtTCEwJx0xUr8005atqHpPi',
+    stripeSecretKey: 'sk_test_51NEG1xSBK5SfV7xSoAiiBa9gWHsrJS57C1rA7WVdV83VzW3taNXmfrJWzTAS0i0l1NFJbAcyhfXMUhSCbDi9fdkT00MXxaot8C'
+};
