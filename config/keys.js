752cd04b496424564d789384d50b3b7a66520549
Aadhaar Bhatia <aadhaar.bhatia@non.keysight.com>
2024-01-14 00:58:03 +0530

    using dev keys in prod mode, for simpler way

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,8 @@
 if (process.env.NODE_ENV === 'production') {
     //IN PROD MODE
-    module.exports = require('./prod');
+    //module.exports = require('./prod');
+    //doing same
+    module.exports = require('./dev');
 } else {
     //IN DEV MODE
     module.exports = require('./dev');
